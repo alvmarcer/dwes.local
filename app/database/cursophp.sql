6054313c680a8db9c8dd01cf8ca68502933535a3
alvmarcer <alvmarcer@alu.edu.gva.es>
2025-12-05 01:00:26 +0100

    Base de datos actualizada

--- app/database/cursophp.sql
+++ app/database/cursophp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 20-10-2025 a las 09:40:16
+-- Tiempo de generación: 05-12-2025 a las 00:59:24
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -34,6 +34,13 @@ CREATE TABLE `asociado` (
   `descripcion` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
+--
+-- Volcado de datos para la tabla `asociado`
+--
+
+INSERT INTO `asociado` (`id`, `nombre`, `logo`, `descripcion`) VALUES
+(1, 'Asociado 7', 'Foto de perfil general.png', 'asociadoxx');
+
 -- --------------------------------------------------------
 
 --
@@ -58,6 +65,31 @@ INSERT INTO `categorias` (`id`, `nombre`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `exposiciones`
+--
+
+CREATE TABLE `exposiciones` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `descripcion` varchar(100) NOT NULL,
+  `fechaInicio` date NOT NULL,
+  `fechaFin` date NOT NULL,
+  `activa` tinyint(1) NOT NULL,
+  `idUsuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `exposiciones`
+--
+
+INSERT INTO `exposiciones` (`id`, `nombre`, `descripcion`, `fechaInicio`, `fechaFin`, `activa`, `idUsuario`) VALUES
+(2, 'Otra prueba más', 'Esto va?', '2025-09-20', '2025-12-14', 1, 5),
+(3, 'Exposición para la semana', 'Exposición de imágenes de esta semana', '2025-12-01', '2025-12-07', 1, 5),
+(4, 'Exposición de alvarito', 'Exposición to flama', '2025-09-20', '2026-09-20', 0, 6);
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `imagenes`
 --
 
@@ -68,8 +100,60 @@ CREATE TABLE `imagenes` (
   `categoria` int(11) NOT NULL DEFAULT 1,
   `numVisualizaciones` int(11) NOT NULL DEFAULT 0,
   `numLikes` int(11) NOT NULL DEFAULT 0,
-  `numDownloads` int(11) NOT NULL DEFAULT 0
+  `numDownloads` int(11) NOT NULL DEFAULT 0,
+  `idUsuario` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `imagenes`
+--
+
+INSERT INTO `imagenes` (`id`, `nombre`, `descripcion`, `categoria`, `numVisualizaciones`, `numLikes`, `numDownloads`, `idUsuario`) VALUES
+(12, 'Fondo Casa Hora de Aventuras.jpg', 'Foto 12', 0, 0, 0, 0, 5),
+(14, 'Foto de perfil general (2).png', 'foto invisible', 0, 0, 0, 0, 6),
+(15, 'Foto de perfil general.png', 'foto del paco jeje', 0, 0, 0, 0, 5);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `imagenes_exposiciones`
+--
+
+CREATE TABLE `imagenes_exposiciones` (
+  `idImagen` int(11) NOT NULL,
+  `idExposicion` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `imagenes_exposiciones`
+--
+
+INSERT INTO `imagenes_exposiciones` (`idImagen`, `idExposicion`) VALUES
+(12, 2),
+(15, 4),
+(12, 3),
+(15, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`id`, `username`, `password`, `role`) VALUES
+(5, 'paco', '$2y$10$F/6sFc8shw6IGDoMD8uwV.FUO14/2rLvfYuXZsd4PWqVMUUtuZcgu', 'ROLE_ADMIN'),
+(6, 'alvarito', '$2y$10$kX4l3j6DieI4psGLjp7l5ezS0.yCmnLcBdAJ9M49PFfoPiCZXbVnW', 'ROLE_USER');
 
 --
 -- Índices para tablas volcadas
@@ -88,10 +172,23 @@ ALTER TABLE `categorias`
   ADD PRIMARY KEY (`id`,`nombre`);
 
 --
+-- Indices de la tabla `exposiciones`
+--
+ALTER TABLE `exposiciones`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indices de la tabla `imagenes`
 --
 ALTER TABLE `imagenes`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -101,7 +198,7 @@ ALTER TABLE `imagenes`
 -- AUTO_INCREMENT de la tabla `asociado`
 --
 ALTER TABLE `asociado`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `categorias`
@@ -110,10 +207,22 @@ ALTER TABLE `categorias`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
+-- AUTO_INCREMENT de la tabla `exposiciones`
+--
+ALTER TABLE `exposiciones`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT de la tabla `imagenes`
 --
 ALTER TABLE `imagenes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
